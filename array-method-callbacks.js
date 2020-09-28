8167aff5e64c9481be4f494076eebb03ef33cdc1
James Mernin <jamespmernin@gmail.com>
2020-09-28 14:38:21 -0400

    Reworded some function calls

--- array-method-callbacks.js
+++ array-method-callbacks.js
@@ -21,38 +21,38 @@ let capitalizedMinions = minions.map(minion => {
 });
 console.log(capitalizedMinions);
 
-function isCapitalized(word) {
+const isCapitalized = (word) => {
   return word[0].toUpperCase() === word[0];
 }
 
-function isLowercase(word) {
+const isLowercase = (word) => {
   return word[0].toLowerCase() === word[0];
 }
 
-console.log(capitalizedMinions.every(isCapitalized));
+console.log(capitalizedMinions.every((minion) => isCapitalized(minion)));
 
-let actuallyCapitalizedMinions = capitalizedMinions.filter(minion => isCapitalized(minion));
+let actuallyCapitalizedMinions = capitalizedMinions.filter((minion) => isCapitalized(minion));
 console.log(actuallyCapitalizedMinions);
-console.log(actuallyCapitalizedMinions.every(isCapitalized));
+console.log(actuallyCapitalizedMinions.every((minion) => isCapitalized(minion)));
 
 const isKevin = (minion) => minion === 'kevin';
 let kevinIndex = capitalizedMinions.findIndex(isKevin)
 console.log(kevinIndex);
 capitalizedMinions[kevinIndex] = (capitalizedMinions[kevinIndex][0].toUpperCase() + capitalizedMinions[kevinIndex].slice(1));
-console.log(capitalizedMinions.every(isCapitalized));
+console.log(capitalizedMinions.every((minion) => isCapitalized(minion)));
 
-let minionNameLengths = capitalizedMinions.map(minion => minion.length);
+let minionNameLengths = capitalizedMinions.map((minion) => minion.length);
 // console.log(minionNameLengths);
 const reducer = (x, i) => x + i;
 console.log(minionNameLengths.reduce(reducer));
 
 const sixPlus = (x) => x.length >= 6;
 const sevenPlus = (x) => x.length >= 7;
-console.log(capitalizedMinions.some(sixPlus));
-console.log(capitalizedMinions.some(sevenPlus));
+console.log(capitalizedMinions.some((minion) => sixPlus(minion)));
+console.log(capitalizedMinions.some((minion) => sevenPlus(minion)));
 capitalizedMinions[kevinIndex] = capitalizedMinions[kevinIndex].toLowerCase();
-console.log(capitalizedMinions.some(isCapitalized));
-console.log(capitalizedMinions.some(isLowercase));
+console.log(capitalizedMinions.some((minion) => isCapitalized(minion)));
+console.log(capitalizedMinions.some((minion) => isLowercase(minion)));
 
 let sortedMinions = capitalizedMinions.sort(function (a, b) {
   // To sort ascending  -> a.length - b.length
